290884663eb26f3bfee3f391849eef1850289ef5
1004491545@qq.com <1004491545@qq.com>
2019-01-09 15:24:26 +0800

    init

---
+++ musicgame/data.js
@@ -0,0 +1,53 @@
+var mediaData = [{
+    soure: "music/夏雨菲 - 沙漠骆驼.mp3",
+    but: [{
+        title: "听前奏识别歌曲",
+        start: 3,
+        end: 24,
+        after: "沙漠骆驼"
+    },
+    {
+        title: "听高潮唱出下一句",
+        start: 59.5,
+        end: 68,
+        after: "什么鬼魅传说 什么魑魅魍魉妖魔 只有那鹭鹰在幽幽的高歌"
+    },
+    {
+        title: "下一句",
+        start: 67.5,
+        end: 77,
+        after: "漫天黄沙掠过 走遍每个角落 行走在无尽的苍茫星河"
+    }
+    ]
+},
+{
+    soure: "music/李玉刚 - 刚好遇见你.mp3",
+    but: [{
+        title: "高潮",
+        start: 36,
+        end: 40,
+        after: ""
+    }]
+},
+{
+    soure: "music/火箭少女101 - 卡路里.mp3",
+    but: [{
+        title: "听前奏识别歌曲",
+        start: 1,
+        end: 17,
+        after: "火箭少女101 - 卡路里"
+    },
+    {
+        title: "听高潮唱出下一句",
+        start: 68.4,
+        end: 73,
+        after: "拜拜 甜甜圈</br> 珍珠奶茶方便面<br/>火锅米饭大盘鸡"
+    },
+    {
+        title: "下一句",
+        start: 73.7,
+        end: 75,
+        after: "拿走拿走别客气"
+    }]
+}
+]
